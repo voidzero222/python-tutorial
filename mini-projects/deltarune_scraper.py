1d7ec5213ad7eb4ff609c17388add30049ca0809
voidzero222 <voidzeroxdd@proton.me>
2025-05-09 17:29:18 +0200

    Made a purely theoretical deltarune-scraper.

--- mini-projects/deltarune_scraper.py
+++ mini-projects/deltarune_scraper.py
@@ -1,3 +1,7 @@
+"""
+For legal reasons this is a project solely for educational purposes that will not be used.
+"""
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -8,4 +12,25 @@ soup = BeautifulSoup(text, features="html.parser")
 
 links = soup.find_all("link", itemprop="associatedMedia")
 
-[print(link) for link in links]
+
+filtered_links = [link for link in links if link["href"].endswith(".m4a") or link["href"].endswith(".mp3")]
+
+
+# for link in filtered_links:
+#     piracy = requests.get(link["href"])
+#     raw = piracy.raw 
+
+names = soup.find_all("meta", itemprop="name")
+
+
+for i in range(0,len(names)):
+    link = filtered_links[i]
+    name = names[i]
+    piracy = requests.get(link["href"])
+    raw = piracy.raw
+    if link["href"].endswith(".m4a"):
+        ext = ".m4a"
+    elif link["href"].endswith(".mp3"):
+        ext = ".mp3"
+    with open(f"home/void/Music/Deltarune/{name["content"]}{ext}","wb") as file:
+        file.write(raw)
