16a4b03baeebd4dc4721444338bc6fa96d45e8f8
Dominik <dominik.reinartz@proton.me>
2025-05-09 17:35:26 +0200

    Fixed deltarune_scraper (but didnt test it)

--- mini-projects/deltarune_scraper.py
+++ mini-projects/deltarune_scraper.py
@@ -4,6 +4,7 @@ For legal reasons this is a project solely for educational purposes that will no
 
 import requests
 from bs4 import BeautifulSoup
+from pathlib import Path
 
 response = requests.get("https://archive.org/details/deltaruneost/")
 text = response.text
@@ -27,10 +28,10 @@ for i in range(0,len(names)):
     link = filtered_links[i]
     name = names[i]
     piracy = requests.get(link["href"])
-    raw = piracy.raw
+    content = piracy.content
     if link["href"].endswith(".m4a"):
         ext = ".m4a"
     elif link["href"].endswith(".mp3"):
         ext = ".mp3"
-    with open(f"home/void/Music/Deltarune/{name["content"]}{ext}","wb") as file:
-        file.write(raw)
+    with open(Path.home() / f"Music/Deltarune/{name["content"]}{ext}","wb") as file:
+        file.write(content)
