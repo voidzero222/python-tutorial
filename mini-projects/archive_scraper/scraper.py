e25ace152b347ab76ab7f81375fe7ce015a14716
voidzero222 <voidzeroxdd@proton.me>
2025-07-04 17:11:13 +0200

    scraper

--- mini-projects/archive_scraper/scraper.py
+++ mini-projects/archive_scraper/scraper.py
@@ -8,35 +8,34 @@ from pathlib import Path
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get("https://archive.org/details/deltaruneost/")
-text = response.text
 
-soup = BeautifulSoup(text, features="html.parser")
+def scrape_archive(url: str, output_directory: Path) -> None:
+    response = requests.get(url)
+    text = response.text
 
-links = soup.find_all("link", itemprop="associatedMedia")
+    soup = BeautifulSoup(text, features="html.parser")
 
+    links = soup.find_all("link", itemprop="associatedMedia")
 
-filtered_links = [
-    link
-    for link in links
-    if link["href"].endswith(".m4a") or link["href"].endswith(".mp3")
-]
+    filtered_links = [
+        link
+        for link in links
+        if link["href"].endswith(".m4a") or link["href"].endswith(".mp3")
+    ]
 
+    names = soup.find_all("meta", itemprop="name")
 
-names = soup.find_all("meta", itemprop="name")
-
-
-for i in range(0, len(names)):
-    link = filtered_links[i]
-    name = names[i]
-    piracy = requests.get(link["href"])
-    content = piracy.content
-    if link["href"].endswith(".m4a"):
-        ext = ".m4a"
-    elif link["href"].endswith(".mp3"):
-        ext = ".mp3"
-    with open(Path.home() / f"Music/Deltarune/{name["content"]}{ext}", "wb") as file:
-        file.write(content)
+    for i in range(0, len(names)):
+        link = filtered_links[i]
+        name = names[i]
+        piracy = requests.get(link["href"])
+        content = piracy.content
+        if link["href"].endswith(".m4a"):
+            ext = ".m4a"
+        elif link["href"].endswith(".mp3"):
+            ext = ".mp3"
+        with open(output_directory / f"{name["content"]}{ext}", "wb") as file:
+            file.write(content)
 
 
 # To convert the files later:
