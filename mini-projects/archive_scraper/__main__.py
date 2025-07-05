010b8bb11cb897d07231d8c131f9562065863f62
voidzero222 <voidzeroxdd@proton.me>
2025-07-05 18:28:17 +0200

    Finished scraper

--- mini-projects/archive_scraper/__main__.py
+++ mini-projects/archive_scraper/__main__.py
@@ -1,5 +1,8 @@
 import argparse
 from pathlib import Path
+import sys
+
+from .scraper import scrape_archive  # type: ignore
 
 __version__ = (0, 1, 0)
 version_string = ".".join(map(str, __version__))
@@ -20,9 +23,9 @@ def main() -> None:
     
     parser.add_argument(
         "-o",
-        "--output_directory",
+        "--output-directory",
         action = "store",
-        default = "/home/Videos",
+        default = Path.home() / "Music",
         help = "The directory for the files to be scraped to.",
         dest = "output_directory",
     )
@@ -42,5 +45,18 @@ def main() -> None:
     url: str = args.url
     output_dir = Path(args.output_directory).absolute().resolve()
     
+    if not url.startswith("https://archive.org"):
+        print(f"{url} is not a valid URL, try again.")
+        sys.exit(1)
+        
+    try:
+        scrape_archive(url, output_dir)
+    except FileNotFoundError:
+        print(f"{output_dir} is an invalid directory, try again.")
+        sys.exit(2)
+    except (PermissionError, OSError):
+        print(f"No write permissions for: {output_dir}. Try again.")
+        sys.exit(3)
+
 if __name__ == "__main__":
     main()
